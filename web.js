7a3d17eb04b4a6fce0586abeffc92426fce036bc
Ubuntu <ubuntu@ip-172-31-38-68.us-west-2.compute.internal>
2013-07-11 02:23:49 +0000

    second attempt

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require('fs');
+var theIndexfile ="index.html";
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html','utf8'));
+  response.send(fs.readFileSync(theIndexfile,'utf8'));
 });
 
 var port = process.env.PORT || 5000;
