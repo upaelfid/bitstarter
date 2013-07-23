8af594b174e89b8df5de8fb1b71a1de022fbf1ac
Ubuntu <ubuntu@ip-172-31-38-68.us-west-2.compute.internal>
2013-07-23 02:20:32 +0000

    finished fancy fluid flayout and added real grader.js

--- grader.js
+++ grader.js
@@ -14,6 +14,7 @@ References:
  + commander.js
    - https://github.com/visionmedia/commander.js
    - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+   - https://github.com/danwrong/restler
 
  + JSON
    - http://en.wikipedia.org/wiki/JSON
@@ -26,6 +27,9 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
+var rest = require('restler');
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,7 +69,13 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'Url to html file')
         .parse(process.argv);
+	if(program.url){
+		var url = program.url.toString();
+		rest.get(url).on('complete', function(result, response){
+			console.log(result);
+		});}
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
